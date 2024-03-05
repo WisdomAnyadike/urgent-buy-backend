467a2ef0744521742416cbe7a3f67a3b79abbea7
WisdomAnyadike <anyadikewisdom@icloud.com>
2024-03-05 19:51:33 +0100

    fixed fuction errors

--- Routes/Productrouts.js
+++ Routes/Productrouts.js
@@ -7,5 +7,5 @@ Router2.post('/createProduct' , createProduct)
 Router2.get('/getProducts' , getList)
 Router2.get('/getProductById/:id' , getProductById)
 Router2.get('/getProductsByCategory/:category' , getProductByCategory)
-Router2.post('/deleteProduct' , deleteProduct)
+Router2.post('/deleteProduct/:id' , deleteProduct)
 module.exports = Router2
