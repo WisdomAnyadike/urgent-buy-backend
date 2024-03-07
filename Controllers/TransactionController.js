4b87657f5caaff2c2c29a5e3e19b2fd445e01476
WisdomAnyadike <anyadikewisdom@icloud.com>
2024-03-07 10:06:28 +0100

    added more get transaction functions

--- Controllers/TransactionController.js
+++ Controllers/TransactionController.js
@@ -79,4 +79,114 @@ const getTransactions = async(req , res)=> {
     }
 }
 
-module.exports = {CreateTransaction , getTransactions}
+const getTransactionsByDateRange = async ( startDate, endDate) =>  {
+    try {
+        const transactions = await transactionModel.find({
+            createdAt: {
+                $gte: startDate,
+                $lt: endDate
+            }
+        });
+        return transactions
+    } catch (error) {
+        res.status(400).send({message:'internal server error ' , status: false })
+        console.log( 'error getting transaction by day' , error)
+    }
+}
+
+const getTodaysTransactions = async (req, res) => {
+       try {
+        const startDate = new Date();
+        startDate.setHours(0, 0, 0, 0); // Set start time to the beginning of today
+        const endDate = new Date();
+        endDate.setHours(23, 59, 59, 999); // Set end time to the end of today
+        const transactions = await getTransactionsByDateRange(startDate, endDate);
+        if(!transactions){
+            res.status(400).send({message:'couldnt get todays transactions' , status: false })   
+        }
+        res.status(200).send({ message: 'Todays transactions fetched successfully', status: 'okay', data: transactions });
+    } catch (error) {
+        res.status(500).send({ message: 'Internal server error', status: false });
+    }
+}
+
+const getYesterdaysTransactions = async (req, res) => {
+    try {
+        const startDate = new Date();
+        startDate.setDate(startDate.getDate() - 1); // Set start date to yesterday
+        startDate.setHours(0, 0, 0, 0);
+        const endDate = new Date();
+        endDate.setDate(endDate.getDate() - 1); // Set end date to yesterday
+        endDate.setHours(23, 59, 59, 999);
+        const transactions = await getTransactionsByDateRange(startDate, endDate);
+        if(!transactions){
+            res.status(400).send({message:'couldnt get yesterdays transactions' , status: false })   
+        }
+        res.status(200).send({ message: 'yesterdays transactions fetched successfully', status: 'okay', data: transactions });    
+    } catch (error) {
+        res.status(500).send({ message: 'Internal server error', status: false });
+    }
+  
+}
+
+const getDayBeforeYesterdaysTransactions = async (req,res) => {
+    try {
+        const startDate = new Date();
+    startDate.setDate(startDate.getDate() - 2); // Set start date to the day before yesterday
+    startDate.setHours(0, 0, 0, 0);
+    const endDate = new Date();
+    endDate.setDate(endDate.getDate() - 2); // Set end date to the day before yesterday
+    endDate.setHours(23, 59, 59, 999);
+    const transactions = await getTransactionsByDateRange(startDate, endDate);
+    if(!transactions){
+        res.status(400).send({message:'couldnt get dayBeforeYesterdays transactions' , status: false })   
+    }
+    res.status(200).send({ message: 'dayBeforeYesterdays transactions fetched successfully', status: 'okay', data: transactions }); 
+    } catch (error) {
+        res.status(500).send({ message: 'Internal server error', status: false });
+    }
+    
+}
+
+const getDayBeforeDayBeforeYesterdaysTransactions = async (req, res) => {
+    try {
+        const startDate = new Date();
+        startDate.setDate(startDate.getDate() - 3); // Set start date to the day before yesterday
+        startDate.setHours(0, 0, 0, 0);
+        const endDate = new Date();
+        endDate.setDate(endDate.getDate() - 3); // Set end date to the day before yesterday
+        endDate.setHours(23, 59, 59, 999);
+        const transactions = await getTransactionsByDateRange(startDate, endDate);
+        if(!transactions){
+            res.status(400).send({message:'couldnt get daybeforeDayBeforeyesterdays transactions' , status: false })   
+        }
+        res.status(200).send({ message: 'daybeforeDayBeforeyesterdays transactions fetched successfully', status: 'okay', data: transactions }); 
+        
+    } catch (error) {
+        res.status(500).send({ message: 'Internal server error', status: false });
+    }
+   
+}
+
+const getDay5Transactions = async (req, res) => {
+    try {
+        const startDate = new Date();
+        startDate.setDate(startDate.getDate() - 4); // Set start date to the day before yesterday
+        startDate.setHours(0, 0, 0, 0);
+        const endDate = new Date();
+        endDate.setDate(endDate.getDate() - 4); // Set end date to the day before yesterday
+        endDate.setHours(23, 59, 59, 999);
+        const transactions = await getTransactionsByDateRange(startDate, endDate);
+        if(!transactions){
+            res.status(400).send({message:'couldnt get lastday transactions' , status: false })   
+        }
+        res.status(200).send({ message: 'Day5 transactions fetched successfully', status: 'okay', data: transactions }); 
+        
+    } catch (error) {
+        res.status(500).send({ message: 'Internal server error', status: false });
+    }
+   
+}
+
+
+module.exports = {CreateTransaction , getTransactions ,getTodaysTransactions, getYesterdaysTransactions, getDayBeforeYesterdaysTransactions , getDayBeforeDayBeforeYesterdaysTransactions , getDay5Transactions}
--- Routes/Transactionroutes.js
+++ Routes/Transactionroutes.js
@@ -1,8 +1,13 @@
 const express = require('express')
-const { getTransactions, CreateTransaction } = require('../Controllers/TransactionController')
+const { getTransactions, CreateTransaction, getTodaysTransactions, getYesterdaysTransactions, getDayBeforeYesterdaysTransactions, getDayBeforeDayBeforeYesterdaysTransactions, getDay5Transactions } = require('../Controllers/TransactionController')
 
 const Router4 = express.Router()
 
 Router4.post('/createTransaction' , CreateTransaction )
 Router4.get('/getTransactions' , getTransactions)
+Router4.get('/getTransactions1' , getTodaysTransactions)
+Router4.get('/getTransactions2' , getYesterdaysTransactions)
+Router4.get('/getTransactions3' , getDayBeforeYesterdaysTransactions)
+Router4.get('/getTransactions4' , getDayBeforeDayBeforeYesterdaysTransactions)
+Router4.get('/getTransactions5' , getDay5Transactions)
 module.exports = Router4
