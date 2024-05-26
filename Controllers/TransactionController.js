7feae319df03ffed8005d3786983997509aebbc3
WisdomAnyadike <anyadikewisdom@icloud.com>
2024-05-26 09:09:21 +0100

    fixed bugs

--- Controllers/TransactionController.js
+++ Controllers/TransactionController.js
@@ -5,77 +5,77 @@ const paystackSecret = process.env.paystackSecret
 
 const CreateTransaction = async (req, res) => {
     const { transactionReference } = req.body
-    if(  !transactionReference  ){
-        res.status(400).send({message:" reference is not provided"})
-    }else{
+    if (!transactionReference) {
+        res.status(400).send({ message: " reference is not provided" })
+    } else {
 
         try {
 
-      const paymentResponse = await axios.get(`https://api.paystack.co/transaction/verify/${transactionReference}` , {
-    headers: {
-        authorization: `Bearer ${paystackSecret}`,
-        "content-type": "application/json",
-        "cache-control": "no control" 
-    },
-        
-})
+            const paymentResponse = await axios.get(`https://api.paystack.co/transaction/verify/${transactionReference}`, {
+                headers: {
+                    authorization: `Bearer ${paystackSecret}`,
+                    "content-type": "application/json",
+                    "cache-control": "no control"
+                },
 
-console.log('payment response:' , paymentResponse.data);
+            })
 
-if(paymentResponse.data.data.status !== 'success'){
-    res.status(400).send({message:" Oops , Payment not completed , try again" , status:false })
-  
-} else {
+            console.log('payment response:', paymentResponse.data);
 
-    try {
-        const createdTransaction = await transactionModel.create({
-            transactionAmount: (paymentResponse.data.data.amount / 100) , 
-            transactionReference : transactionReference , 
-             transactionStatus : paymentResponse.data.data.status,   
-             transactionUser : paymentResponse.data.data.metadata.FullName , 
-             transactionType : "credit"
-        })
+            if (paymentResponse.data.data.status !== 'success') {
+                res.status(400).send({ message: " Oops , Payment not completed , try again", status: false })
 
-        if(!createdTransaction){
-            res.status(400).send({message:"Unable to create transaction" , status:false})   
-        }else{
-            res.status(200).send({  message: 'Verification successful', status:'okay' , createdTransaction }) 
-            console.log('Created Transaction', createdTransaction );
-        }
-        
-    } catch (error) {
-        res.status(500).send({message:"Internal server error" , status:false})  
-        console.log('error while creating transaction' , error);
-    }
+            } else {
 
-}
-            
+                try {
+                    const createdTransaction = await transactionModel.create({
+                        transactionAmount: (paymentResponse.data.data.amount / 100),
+                        transactionReference: transactionReference,
+                        transactionStatus: paymentResponse.data.data.status,
+                        transactionUser: paymentResponse.data.data.metadata.FullName,
+                        transactionType: "credit"
+                    })
+
+                    if (!createdTransaction) {
+                        res.status(400).send({ message: "Unable to create transaction", status: false })
+                    } else {
+                        res.status(200).send({ message: 'Verification successful', status: 'okay', createdTransaction })
+                        console.log('Created Transaction', createdTransaction);
+                    }
+
+                } catch (error) {
+                    res.status(500).send({ message: "Internal server error", status: false })
+                    console.log('error while creating transaction', error);
+                }
+
+            }
+
         } catch (error) {
-            res.status(500).send({message:error.response.data.message , status:false})  
-            console.log('error while getting from paystack' , error.response.data); 
-        }  
-     
-    }
-
-}
-
-
-const getTransactions = async(req , res)=> {
-    try {
-   const transactions = await transactionModel.find()
-        if(!transactions){
-            res.status(400).send({message:'couldnt fetch transactions' , status:false})
-        }else{
-            res.status(200).send({message:'Transactions fetched successfully' , status:'okay' ,  data:transactions })
+            res.status(500).send({ message: error.response.data.message, status: false })
+            console.log('error while getting from paystack', error.response.data);
         }
-        
+
+    }
+
+}
+
+
+const getTransactions = async (req, res) => {
+    try {
+        const transactions = await transactionModel.find()
+        if (!transactions) {
+            res.status(400).send({ message: 'couldnt fetch transactions', status: false })
+        } else {
+            res.status(200).send({ message: 'Transactions fetched successfully', status: 'okay', data: transactions })
+        }
+
     } catch (error) {
-        res.status(500).send({message:'Internal server error' , status:false }) 
-        console.log('fetching error' , error);
+        res.status(500).send({ message: 'Internal server error', status: false })
+        console.log('fetching error', error);
     }
 }
 
-const getTransactionsByDateRange = async ( startDate, endDate) =>  {
+const getTransactionsByDateRange = async (startDate, endDate) => {
     try {
         const transactions = await transactionModel.find({
             createdAt: {
@@ -85,20 +85,20 @@ const getTransactionsByDateRange = async ( startDate, endDate) =>  {
         });
         return transactions
     } catch (error) {
-        res.status(400).send({message:'internal server error ' , status: false })
-        console.log( 'error getting transaction by day' , error)
+        res.status(400).send({ message: 'internal server error ', status: false })
+        console.log('error getting transaction by day', error)
     }
 }
 
 const getTodaysTransactions = async (req, res) => {
-       try {
+    try {
         const startDate = new Date();
         startDate.setHours(0, 0, 0, 0); // Set start time to the beginning of today
         const endDate = new Date();
         endDate.setHours(23, 59, 59, 999); // Set end time to the end of today
         const transactions = await getTransactionsByDateRange(startDate, endDate);
-        if(!transactions){
-            res.status(400).send({message:'couldnt get todays transactions' , status: false })   
+        if (!transactions) {
+            res.status(400).send({ message: 'couldnt get todays transactions', status: false })
         }
         res.status(200).send({ message: 'Todays transactions fetched successfully', status: 'okay', data: transactions });
     } catch (error) {
@@ -115,33 +115,33 @@ const getYesterdaysTransactions = async (req, res) => {
         endDate.setDate(endDate.getDate() - 1); // Set end date to yesterday
         endDate.setHours(23, 59, 59, 999);
         const transactions = await getTransactionsByDateRange(startDate, endDate);
-        if(!transactions){
-            res.status(400).send({message:'couldnt get yesterdays transactions' , status: false })   
+        if (!transactions) {
+            res.status(400).send({ message: 'couldnt get yesterdays transactions', status: false })
         }
-        res.status(200).send({ message: 'yesterdays transactions fetched successfully', status: 'okay', data: transactions });    
+        res.status(200).send({ message: 'yesterdays transactions fetched successfully', status: 'okay', data: transactions });
     } catch (error) {
         res.status(500).send({ message: 'Internal server error', status: false });
     }
-  
+
 }
 
-const getDayBeforeYesterdaysTransactions = async (req,res) => {
+const getDayBeforeYesterdaysTransactions = async (req, res) => {
     try {
         const startDate = new Date();
-    startDate.setDate(startDate.getDate() - 2); // Set start date to the day before yesterday
-    startDate.setHours(0, 0, 0, 0);
-    const endDate = new Date();
-    endDate.setDate(endDate.getDate() - 2); // Set end date to the day before yesterday
-    endDate.setHours(23, 59, 59, 999);
-    const transactions = await getTransactionsByDateRange(startDate, endDate);
-    if(!transactions){
-        res.status(400).send({message:'couldnt get dayBeforeYesterdays transactions' , status: false })   
-    }
-    res.status(200).send({ message: 'dayBeforeYesterdays transactions fetched successfully', status: 'okay', data: transactions }); 
+        startDate.setDate(startDate.getDate() - 2); // Set start date to the day before yesterday
+        startDate.setHours(0, 0, 0, 0);
+        const endDate = new Date();
+        endDate.setDate(endDate.getDate() - 2); // Set end date to the day before yesterday
+        endDate.setHours(23, 59, 59, 999);
+        const transactions = await getTransactionsByDateRange(startDate, endDate);
+        if (!transactions) {
+            res.status(400).send({ message: 'couldnt get dayBeforeYesterdays transactions', status: false })
+        }
+        res.status(200).send({ message: 'dayBeforeYesterdays transactions fetched successfully', status: 'okay', data: transactions });
     } catch (error) {
         res.status(500).send({ message: 'Internal server error', status: false });
     }
-    
+
 }
 
 const getDayBeforeDayBeforeYesterdaysTransactions = async (req, res) => {
@@ -153,15 +153,15 @@ const getDayBeforeDayBeforeYesterdaysTransactions = async (req, res) => {
         endDate.setDate(endDate.getDate() - 3); // Set end date to the day before yesterday
         endDate.setHours(23, 59, 59, 999);
         const transactions = await getTransactionsByDateRange(startDate, endDate);
-        if(!transactions){
-            res.status(400).send({message:'couldnt get daybeforeDayBeforeyesterdays transactions' , status: false })   
+        if (!transactions) {
+            res.status(400).send({ message: 'couldnt get daybeforeDayBeforeyesterdays transactions', status: false })
         }
-        res.status(200).send({ message: 'daybeforeDayBeforeyesterdays transactions fetched successfully', status: 'okay', data: transactions }); 
-        
+        res.status(200).send({ message: 'daybeforeDayBeforeyesterdays transactions fetched successfully', status: 'okay', data: transactions });
+
     } catch (error) {
         res.status(500).send({ message: 'Internal server error', status: false });
     }
-   
+
 }
 
 const getDay5Transactions = async (req, res) => {
@@ -173,15 +173,15 @@ const getDay5Transactions = async (req, res) => {
         endDate.setDate(endDate.getDate() - 4); // Set end date to the day before yesterday
         endDate.setHours(23, 59, 59, 999);
         const transactions = await getTransactionsByDateRange(startDate, endDate);
-        if(!transactions){
-            res.status(400).send({message:'couldnt get lastday transactions' , status: false })   
+        if (!transactions) {
+            res.status(400).send({ message: 'couldnt get lastday transactions', status: false })
         }
-        res.status(200).send({ message: 'Day5 transactions fetched successfully', status: 'okay', data: transactions }); 
-        
+        res.status(200).send({ message: 'Day5 transactions fetched successfully', status: 'okay', data: transactions });
+
     } catch (error) {
         res.status(500).send({ message: 'Internal server error', status: false });
     }
-   
+
 }
 
 
@@ -192,18 +192,18 @@ const getLastThirtyDaysTransactions = async (req, res) => {
         startDate.setHours(0, 0, 0, 0);
         const endDate = new Date();
         endDate.setHours(23, 59, 59, 999);  // Set end date to today
-    
+
         const transactions = await getTransactionsByDateRange(startDate, endDate);
-        if(!transactions){
-            res.status(400).send({message:'couldnt get yesterdays transactions' , status: false })   
+        if (!transactions || transactions.length === 0) {
+            return res.status(200).send({ message: 'No transactions found for the last thirty days', status: 'okay', data: [] });
         }
-        res.status(200).send({ message: 'transactions from the last thirty day fetched successfully', status: 'okay', data: transactions });    
+        res.status(200).send({ message: 'transactions from the last thirty day fetched successfully', status: 'okay', data: transactions });
     } catch (error) {
         res.status(500).send({ message: 'Internal server error', status: false });
     }
-  
+
 }
 
 
-module.exports = {CreateTransaction , getTransactions ,getTodaysTransactions, getYesterdaysTransactions, getDayBeforeYesterdaysTransactions , getDayBeforeDayBeforeYesterdaysTransactions , getDay5Transactions , getLastThirtyDaysTransactions }
+module.exports = { CreateTransaction, getTransactions, getTodaysTransactions, getYesterdaysTransactions, getDayBeforeYesterdaysTransactions, getDayBeforeDayBeforeYesterdaysTransactions, getDay5Transactions, getLastThirtyDaysTransactions }
 
